7b59bf87e76c8250dfa93559e97dd26ea82ca6b5
Carlos David Barreiros Reyes <davizi96@hotmail.com>
2025-06-04 23:02:06 -0300

    feat: backend setup

--- import/install.go
+++ import/install.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	// Database connection string for your remote PostgreSQL database
-	connStr := "postgres://username:password@remote-host:port/database?sslmode=disable"
+	connStr := "postgresql://moviesdb_owner:npg_PIkfU3DzTlW7@ep-bold-fire-a86kh6bq-pooler.eastus2.azure.neon.tech/moviesdb?sslmode=require"
 
 	// Open database connection
 	db, err := sql.Open("postgres", connStr)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"frontendmasters.com/movies/data"
+	"frontendmasters.com/movies/handlers"
+	"frontendmasters.com/movies/logger"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	logInstance.Error("Hello from the Error system", nil)
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
+
+func main() {
+
+	logInstance := initializeLogger()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file was available")
+	}
+
+	dbConnStr := os.Getenv("DATABASE_URL")
+	if dbConnStr == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+	db, err := sql.Open("postgres", dbConnStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the DB: %v", err)
+	}
+	defer db.Close()
+
+	movieRepo, err := data.NewMovieRepository(db, logInstance)
+	if err != nil {
+		log.Fatalf("Failed to initialize repository")
+	}
+
+	movieHandler := handlers.NewMovieHandler(movieRepo, logInstance)
+
+
+	http.HandleFunc("/api/movies/top", movieHandler.GetTopMovies)
+	http.HandleFunc("/api/movies/random", movieHandler.GetRandomMovies)
+	http.HandleFunc("/api/movies/search", movieHandler.SearchMovies)
+	http.HandleFunc("/api/movies/", movieHandler.GetMovie)
+	http.HandleFunc("/api/genres", movieHandler.GetGenres)
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	fmt.Println("Server is running on http://localhost:8080")
+
+	const addr = ":8080"
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
+	}
+}
